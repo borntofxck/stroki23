afc898f8e66cb91af889264ba653736b9f9ae86f
kvtsuki <117033172+kvtsuki@users.noreply.github.com>
2023-10-01 22:25:29 +0500

    Добавьте файлы проекта.

---
+++ ConsoleApp28/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp28
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("задание 2");
+            string x = "это тест", x1, x2, x3;
+            Console.WriteLine("Исходная строка: " + x);
+            x1 = x.Insert(4, "простой ");
+            Console.WriteLine("Новая строка: " + x1);
+
+
+            Console.WriteLine("задание 3");
+            Console.WriteLine("Исходная строка: " + x1);
+            x2 = x1.Insert(4, "не");
+            Console.WriteLine("Новая строка: " + x2);
+            x3 = x1.Insert(4, "но");
+            Console.WriteLine("Новая строка: " + x3);
+        }
+    }
+}
